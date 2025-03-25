afa564e48c65027ffa90b1c2ef52524d00a2a4a6
Pallawi Singh <163420630+pallawi-singh@users.noreply.github.com>
2025-03-25 17:38:59 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,7 +1,7 @@
 import profilepic from './profile-pic.png'
 import upload_icon from './upload-icon.png'
 
-export const url='https://luxe-backendfile.onrender.com'
+export const url='https://luxe-backendfile.onrender.com/'
 export const assets = {
     profilepic,
     upload_icon,
