3e8285767d7e7aa67937668366e49ef57d8b41c9
pallawi singh <pallawikumari2403@gmail.com>
2025-03-25 15:48:42 +0530

    change bycript

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import validator from "validator"
 
 
@@ -11,45 +11,44 @@ const createToken = (id) => {
 
 
 // resister user
-const registerUser = async (req,res) => {
+const registerUser = async (req, res) => {
     const { name, password, email } = req.body;
 
     try {
         const exists = await userModel.findOne({ email });
 
-        // checking if user exist
         if (exists) {
-            return res.json({ success: false, message: "User Already Exist" })
+            return res.json({ success: false, message: "User Already Exists" });
         }
 
-        // validating email and password
         if (!validator.isEmail(email)) {
-            return res.json({ success: false, message: "Please Enter a valid email" })
+            return res.json({ success: false, message: "Please enter a valid email" });
         }
 
         if (password.length < 8) {
-            return res.json({ success: false, message: "Please Enter a strong password" })
+            return res.json({ success: false, message: "Please enter a strong password" });
         }
 
-        // hasing user password
-        const salt = await bcrypt.genSalt(10)
-        const hasedPaasword = await bcrypt.hash(password, salt)
+        // Corrected bcrypt usage
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         const newUser = new userModel({
-            name: name,
-            email: email,
-            password: hasedPaasword
-        })
+            name,
+            email,
+            password: hashedPassword // Fixed variable name
+        });
 
-        const user = await newUser.save()
-        const token = createToken(user._id)
-        res.json({ success: true, token })
+        const user = await newUser.save();
+        const token = createToken(user._id);
+        res.json({ success: true, token });
 
     } catch (error) {
         console.log(error);
-        res.json({ success: false, message: "Error" })
+        res.json({ success: false, message: "Error" });
     }
-}
+};
+
 
 
 
