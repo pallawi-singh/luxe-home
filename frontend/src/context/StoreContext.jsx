feadf97173410f85df6e17a836986ee14dcd2322
Pallawi Singh <163420630+pallawi-singh@users.noreply.github.com>
2025-03-25 16:15:21 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartProduct, setCartProduct] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://luxe-backendfile.onrender.com";
     const [token, setToken] = useState("");
 
     const [product_list, setProductList] = useState([]);
